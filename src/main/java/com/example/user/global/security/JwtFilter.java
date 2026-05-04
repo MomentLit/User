759c05f033d471b280a6c74e8508ada0e8ae5d27
주환 <jhlarry1109@gmail.com>
2026-05-04 16:25:34 +0900

    feat: jwt 인증 구조 / controller 추가

---
+++ src/main/java/com/example/user/global/security/JwtFilter.java
@@ -0,0 +1,30 @@
+package com.example.user.global.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class UserAuthenticationFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+        throws ServletException, IOException {
+        String token = request.getHeader("Authorization");
+
+        if (token != null && token.startsWith("Bearer ")) {
+            token = token.substring(7);
+
+            String userId = jwtProvider.get
+        }
+    }
+}
---
+++ src/main/java/com/example/user/global/security/JwtProvider.java
@@ -0,0 +1,4 @@
+package com.example.user.global.security;
+
+public class JwtProvider {
+}
